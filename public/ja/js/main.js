02a10521230ceb9f7cf866a758ecb06deaac4bca
nakajimamasao <chelseav3@hotmail.com>
2022-08-15 11:48:41 +0900

    v.1.3.3_letselevator

--- public/ja/js/main.js
+++ public/ja/js/main.js
@@ -39,12 +39,28 @@
   }
 }
 
-function footer(){
-  $.ajax({
-    url: "footer.html",
-    cache: false,
-    success: function(html){
-      document.write(html);
+// 下にスクロールしてバナーボタンを表示
+$(function(){
+  var bottom = $('.banner_button');
+  var content = $('.content');
+  $(window).on('scroll', function(){
+
+    if($(this).scrollTop() > window.innerHeight / 2){
+      //上にスクロールしたとき
+      bottom.removeClass('hide');
+    }else{
+      //下にスクロールしたとき
+      bottom.addClass('hide');
+    }
+
+    if (window.innerWidth < 700) {
+      if($(this).scrollTop() > 0){
+          //上にスクロールしたとき
+          content.addClass('active');
+        }else{
+          //下にスクロールしたとき
+          content.removeClass('active');
+        }    
     }
   });
-}
+});
